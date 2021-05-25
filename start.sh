fdd5945433175d41c48cac6b3a73c6883e7d57f9
Kosolov325 <58604882+Kosolov325@users.noreply.github.com>
2021-05-25 19:19:28 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ if ps -ef | grep -v grep | grep screen ; then
   echo "WARN: you can still running another one by starting manually."
   exit 0
 else
-  screen -m -d -S server DreamDaemon tgstation.dmb 7240 -trusted
+  screen -m -d -S server DreamDaemon tgstation.dmb 2505 -trusted
   echo "launching"
   exit 0
 fi
