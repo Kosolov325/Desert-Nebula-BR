36563fa19eafd8ad5639a8d661526412a6b505c0
root <root@vps.southamerica-east1-a.c.mystic-advice-315014.internal>
2021-05-28 23:46:44 +0000

    backup3kkk

--- start.sh
+++ start.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-if ps -ef | grep -v grep | grep screen ; then
-  echo "already running."
-  echo "WARN: you can still running another one by starting manually."
-  exit 0
-else
   screen -m -d -S server DreamDaemon tgstation.dmb 2505 -trusted
   echo "launching"
   exit 0
-fi
